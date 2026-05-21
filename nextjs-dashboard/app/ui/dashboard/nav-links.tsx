490b80ec8c59f7aae3a5862aede7a928f48810b8
Michell herrera <76981040+michell179708@users.noreply.github.com>
2026-05-21 19:19:55 +0200

    changes

--- nextjs-dashboard/app/ui/dashboard/nav-links.tsx
+++ nextjs-dashboard/app/ui/dashboard/nav-links.tsx
@@ -9,8 +9,6 @@ import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import clsx from 'clsx';
 
-// Map of links to display in the side navigation.
-// Depending on the size of the application, this would be stored in a database.
 const links = [
   { name: 'Home', href: '/dashboard', icon: HomeIcon },
   {
@@ -23,12 +21,14 @@ const links = [
 
 export default function NavLinks() {
   const pathname = usePathname();
+
   return (
     <>
       {links.map((link) => {
         const LinkIcon = link.icon;
+
         return (
-        <link
+          <Link
             key={link.name}
             href={link.href}
             className={clsx(
@@ -37,11 +37,10 @@ export default function NavLinks() {
                 'bg-sky-100 text-blue-600': pathname === link.href,
               },
             )}
-            
           >
             <LinkIcon className="w-6" />
             <p className="hidden md:block">{link.name}</p>
-        </link>
+          </Link>
         );
       })}
     </>
